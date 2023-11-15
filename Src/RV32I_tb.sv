895e74689c46662ebdf3be8835b6f521f5f20079
qamar411 <qmoavia.bee19seecs@seecs.edu.pk>
2023-11-15 12:58:09 +0500

    changed testbench to automatic

--- Src/RV32I_tb.sv
+++ Src/RV32I_tb.sv
@@ -9,6 +9,12 @@ RV32I dut(
 	.rst_n(!rst_n)
 	);
 
+reg [`RV_DMEM_WIDTH-1:0] ref_dmem [`RV_DMEM_DEPTH-1:0];
+reg [`RV_RF_WIDTH-1:0] ref_regfile [`RV_RF_DEPTH-1:0];
+reg [4:0]  RF_mismatch = 0;
+reg [31:0] MEM_mismatch = 0;
+
+
 initial
 begin
 	clk=1;
@@ -17,7 +23,11 @@ begin
 	#20
 
 	rst_n = 0;
-	$readmemh("DMEM.s", dut.MEM_inst.DMEM_mem.dmem); // used in 4x4 matrix multiplication	
+	$readmemh("../R_Type/R_type_initial_MEM.s", dut.MEM_inst.DMEM_mem.dmem); // initializing the DMEM here 
+	$readmemh("../R_Type/R_type_register_initial.s", dut.ID_inst.RegFile_Dec.regfile); // initializing the Register file here	
+	$readmemh("../R_Type/R_type_final_MEM.s",ref_dmem ); // saving the final memory of true reference design in reference memory
+	$readmemh("../R_Type/R_type_register_final.s", ref_regfile); // saving the final register file content of true reference design in reference regfile
+
 	// ------------ Start of Simulation --------------------
 	$display($time, " PC value = %d", dut.pc_current_if);
 
@@ -35,12 +45,82 @@ begin
 	begin
 		$display("Value of register %d = %d", i, dut.ID_inst.RegFile_Dec.regfile[i]);
 	end
+
+	// Automatically testing by matching the content of Register File and Data memory with there required values.
+	$display ("");
+	$display ("");	
+	$display ("************************************************************");
+	$display ("********* Starting Register File Content Matching ... ******");
+	$display ("************************************************************");
+	$display ("");
+	$display ("");
+	for(integer i = 0; i < 32; i++)
+	begin
+		if(dut.ID_inst.RegFile_Dec.regfile[i] != ref_regfile[i])
+		begin
+		$display("Error... RegFile[%d] = %d is not equal to Ref_RegFile[%d] = %d", i, dut.ID_inst.RegFile_Dec.regfile[i],i,ref_regfile[i]);
+		RF_mismatch = RF_mismatch + 1;
+		end
+		else
+		$display("Success... RegFile[%d] = %d is equal to Ref_RegFile[%d] = %d", i, dut.ID_inst.RegFile_Dec.regfile[i],i,ref_regfile[i]);
+		
+	end
+	$display ("");
+	$display ("");
+    if(RF_mismatch == 0)
+	begin
+		$display ("************************************************************");
+		$display ("********************** RF Test Passed **********************");
+		$display ("************************************************************");
+	end
+	else
+		begin
+		$display ("************************************************************");
+		$display ("********************** RF Test Failed **********************");
+		$display ("************************************************************");
+
+	end
+	$display ("");
+	$display ("");
+	$display ("************************************************************");
+	$display ("************ Starting Memory Content Matching ... **********");
+	$display ("************************************************************");
+	$display ("");
+	$display ("");
+
+	for(integer i = 0; i < `RV_DMEM_DEPTH; i++)
+	begin
+		if(dut.MEM_inst.DMEM_mem.dmem[i] != ref_dmem[i])
+		begin
+		$display("Error... DMEM[%d] = %d is not equal to Ref_DMEM[%d] = %d", i, dut.MEM_inst.DMEM_mem.dmem[i],i,ref_dmem[i]);
+		RF_mismatch = RF_mismatch + 1;
+		end
+		else
+		begin
+		$display("Success... DMEM[%d] = %d is equal to Ref_DMEM[%d] = %d", i, dut.MEM_inst.DMEM_mem.dmem[i],i,ref_dmem[i]);
+		end
+	end
+	$display ("");
+	$display ("");
+    if(MEM_mismatch == 0)
+	begin
+		$display ("************************************************************");
+		$display ("********************* MEM Test Passed **********************");
+		$display ("************************************************************");
+	end
+	else
+		begin
+		$display ("************************************************************");
+		$display ("********************* MEM Test Failed **********************");
+		$display ("************************************************************");
+	end	
+
 end
 
 // Copying Instruction Machine codes to IMEM
 initial 
 begin
-	$readmemh("IMEM.s", dut.IF_inst.IMEM_.imem);		
+	$readmemh("../R_Type/R_type_machine.s", dut.IF_inst.IMEM_.imem);		
 end
 
 // Clock will toggle iff DMEM last entry is not equal to 32'hFE23
